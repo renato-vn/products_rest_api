790f3de79b38ea7d1cb5f9b8fa9b5671a0599db9
Renato Vicencio N <renatovicencion@gmail.com>
2024-08-25 20:40:59 -0400

    :wrench: chore: update config files

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./router";
 import db from "./config/db";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 
@@ -21,8 +23,23 @@ connectDB();
 // Instancia de express
 const server = express();
 
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de cors"));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
+
 // Leer datos de formularios
 server.use(express.json());
+
+server.use(morgan("dev"));
 
 server.use("/api/products", router);
 
